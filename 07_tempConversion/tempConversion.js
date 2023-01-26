0d067602ecca125e92160f90190c35d9fe72da45
chad-alt <chadallman2016@gmail.com>
2023-01-26 15:46:49 -0500

    completed exercises 6 to 8

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let poop = (fahrenheit - 32) / 1.8
+  return Math.round(poop * 10) / 10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let num = celsius * 9/5 + 32;
+  return Math.round(num * 10) / 10
 };
 
 // Do not edit below this line
@@ -9,3 +13,4 @@ module.exports = {
   convertToCelsius,
   convertToFahrenheit
 };
+  
--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(c, d) {
+	return c - d
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let sum = 0;
+
+  array.forEach(item => {
+    sum += item;
+  });
+
+  console.log(sum);
+  return sum;              
+  //adds arrays up
 };
 
-const multiply = function() {
-
+const multiply = function(myValue) {
+  return myValue.reduce((a,b)=> a*b)
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return a ** b
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0 || num === 1)
+  return 1; 
+  for (var i = num - 1; i >= 1; i--) {
+    num *= i;
+  }
+  return num;
 };
 
 // Do not edit below this line
